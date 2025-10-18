d1e63ddffef7bb9f541079eff492f31e6bfabcad
Sathvik Yechuri <sathvik.y007@gmail.com>
2025-10-18 13:53:32 -0500

    Button test

--- UltrasonicTest/UltrasonicTest.ino
+++ UltrasonicTest/UltrasonicTest.ino
@@ -4,16 +4,25 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET    -1  // No reset pin
+#define OLED_RESET    -1 //null
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define TRIG_PIN 25
-#define ECHO_PIN 33
+#define TRIG_PIN1 18
+#define ECHO_PIN1 19
+#define TRIG_PIN2 2
+#define ECHO_PIN2 4
+
+bool test = false
+
+#define BUTTON_PIN 5
 
 void setup() {
   Serial.begin(115200);
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIG_PIN1, OUTPUT);
+  pinMode(ECHO_PIN1, INPUT);
+  pinMode(TRIG_PIN2, OUTPUT);
+  pinMode(ECHO_PIN2, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("OLED init failed");
@@ -21,9 +30,12 @@ void setup() {
   }
 
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
+  int charWidth = 6; // for size 1
+  int textWidth = text.length() * charWidth;
+  int x = (SCREEN_WIDTH - textWidth) / 2;
+  display.setCursor(x, 28);
   display.println("Starting...");
   display.display();
   delay(1000);
@@ -31,28 +43,39 @@ void setup() {
 
 void loop() {
   // Trigger distance sensor
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+  long duration = 0
+  float distance = 0.0
+  if(!test) {
+    digitalWrite(TRIG_PIN1, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN1, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN1, LOW);
+    long duration = pulseIn(ECHO_PIN1, HIGH, 30000); // 30ms
+    float distance = duration * 0.0343 / 2;
+  } else {
+    digitalWrite(TRIG_PIN2, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN2, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN2, LOW);
+    duration = pulseIn(ECHO_PIN2, HIGH, 30000); // 30ms
+    distance = duration * 0.0343 / 2;
+  }
 
-  // Measure echo
-  long duration = pulseIn(ECHO_PIN, HIGH, 30000); // 30ms
-  float distance = duration * 0.0343 / 2;
+  String text = "Dist: " + String(distance, 1) + " cm";
+  int x = (SCREEN_WIDTH - text.length() * 6) / 2;
 
-  // Serial print
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  // OLED print
   display.clearDisplay();
-  display.setCursor(0, 0);
-  display.print("Dist: ");
-  display.print(distance, 1);
-  display.println(" cm");
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(x, 28); // centring
+  display.print(text);
   display.display();
 
-  delay(500);
+  if (buttonPressed) {
+    test= !test
+  }
+
+  delay(250);
 }
